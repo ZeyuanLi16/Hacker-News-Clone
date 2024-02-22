16c436328bb9204f2610823bd8e8a7cf3d2aa82e
Zeyuan Li <li.ze@husky.neu.edu>
2024-02-22 14:41:21 -0600

    Initial commit

---
+++ pages/stories.js
@@ -0,0 +1,20 @@
+import view from "../utils/view.js"
+
+export default async function Stories(path) {
+    const stories = await getStories(path);
+    const hasStories = stories.length > 0;
+                    
+    view.innerHTML = `<div>
+      ${hasStories ? stories.map(story => JSON.stringify(story)) : 'No stories'}
+    </div>`;  
+}
+
+async function getStories(path) {
+    const isHomeRoute = path === '/';
+    if (isHomeRoute) {
+        path = '/news';  
+    }
+    const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
+    const stories = await response.json();
+    return stories;
+}
